3e98e495e44e1f119c32124c28f28d546ae410ab
ALEKSANDR KOYNOSOV <abenefic@gmail.com>
2021-03-16 13:41:17 +0300

    Initial commit

---
+++ src/main/java/ru/runanddone/admin/RnDAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.runanddone.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnDAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RnDAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/runanddone/admin/RnDAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.runanddone.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnDAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
