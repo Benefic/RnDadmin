abdd2b743a31eb9bd9e99433fc6bada48571f367
ALEKSANDR KOYNOSOV <abenefic@gmail.com>
2021-09-23 16:29:42 +0300

    data mappers removed

--- src/main/java/ru/runanddone/admin/contragents/controller/ContragentController.java
+++ src/main/java/ru/runanddone/admin/contragents/controller/ContragentController.java
@@ -1,6 +1,7 @@
 package ru.runanddone.admin.contragents.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.runanddone.admin.contragents.model.ContragentDto;
 import ru.runanddone.admin.contragents.service.ContragentService;
 
-import java.util.List;
-
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/contragents")
@@ -20,7 +19,7 @@ public class ContragentController {
     private final ContragentService service;
 
     @GetMapping
-    public ResponseEntity<List<ContragentDto>> getAll(@RequestParam(defaultValue = "1") int page,
+    public ResponseEntity<Page<ContragentDto>> getAll(@RequestParam(defaultValue = "1") int page,
                                                       @RequestParam(defaultValue = "10") int size) {
         if (page <= 1) {
             page = 0;
